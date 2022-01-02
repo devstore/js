9254c05e46d029fdad811432ccdb78f2371d8594
Ashutosh Singh <ashugotmail@gmail.com>
2022-01-01 23:18:07 -0800

    Object example added

---
+++ objects/ListObjectProperties.js
@@ -0,0 +1,14 @@
+//List object properties and values
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+  };
+   
+  for (let key in user) {
+    // keys
+    console.log( key );  // name, age, isAdmin
+    // values for the keys
+    console.log( user[key] ); // John, 30, true
+  }
+  
